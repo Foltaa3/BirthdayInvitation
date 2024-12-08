444b6c99ecbc9dbfe17775917950c9060822d002
Fislam <fislam1310@gmail.com>
2024-12-08 20:48:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('rsvpButton').addEventListener('click', function() {
+    document.getElementById('rsvpMessage').innerText = "I hope you come to my birthday party :) !";
+});
